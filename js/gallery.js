89682c6789d9990c08acd7a370a943ff248b54ad
Bevin Hernandez <bevin.hernandez@gmail.com>
2017-09-21 15:07:28 -0400

    refactoring. looking at conditionalizing the forward/back buttons.

--- js/gallery.js
+++ js/gallery.js
@@ -37,39 +37,50 @@
             }
         });
     }
-    
+    function showLightBox(index) {
+         //hide all the figures
+        $('figure').hide();
+        //show just the one you clicked
+        console.log(index);
+        $(index).show();
+        $(index).children().show();
+        //toggle the class on or off
+        $(index).parent().toggleClass('lightbox');
+        $('.lightbox').children().show();
+        //show all the rest of the figures if you click off
+        if (!$(index).parent().hasClass('lightbox')) {
+            $('figure').show();
+            $('figcaption').hide();
+        }
+        //hide the next and previous arrows if you only have one image filtered
+       if (visiblefigures.length === 1) {
+            $('.prev').hide();
+            $('.next').hide();
+       }
+    }
     //check the input field for input, and search the alt and images to see if it's there
     $("input").keyup(function () {
         var inputText = $('input').val();
         searchString(inputText);
         //change the array of visible figures to return only the filtered results
         visiblefigures = $('figure:visible');
-        return visiblefigures
-         
+        
+        visiblefigures.each(function(index) {
+            $(this).on('click', function() {
+                showLightBox(index)
+            })
+        })     
     });
+    
+    
     
     //lightbox show/hide
     $('figure').on('click', function () {
-        //hide all the figures
-        $('figure').hide();
-        //show just the one you clicked
-        $(this).show();
-        $(this).children().show();
-        //toggle the class on or off
-        $(this).parent().addClass('lightbox');
-        $('.lightbox').children().show();
-        //show all the rest of the figures if you click off
-        if (!$(this).parent().hasClass('lightbox')) {
-            $('figure').show();
-            $('figcaption').hide();
-        }
-    
-       if (visiblefigures.length === 1) {
-            $('.prev').hide();
-            $('.next').hide();
-       }
+        var index = $(this);
+        showLightBox(index); 
     });
     
+    //Close function goes on div to remove lightbox if you click anywhere in it
     $('.close').on('click', function () {
         $(this).parent().removeClass('lightbox');
         $('figure').show();
