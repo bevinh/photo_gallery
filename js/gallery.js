9e88a2729ebd6bc2d54de4e7a12a7a5ac002b175
Bevin Hernandez <bevin.hernandez@gmail.com>
2017-09-21 12:39:07 -0400

    lightbox functionality at small resolution

--- js/gallery.js
+++ js/gallery.js
@@ -6,7 +6,7 @@
     $.each(figcaptions, function () {
         $(this).hide();
     });
-    
+    $('.close').hide();
     //function for finding the text in the figures
     function searchString(cap) {
         //Hide all the figures
@@ -35,6 +35,32 @@
     
     //check the input field for input, and search the alt and images to see if it's there
     $("input").keyup(function () {
+        var inputText = $('input').val();
+        searchString(inputText);
+    });
+    
+    //lightbox show/hide
+    $('figure').on('click', function () {
+        //hide all the figures
+        $('figure').hide();
+        //show just the one you clicked
+        $(this).show();
+        $(this).children().show();
+        //toggle the class on or off
+        $(this).parent().addClass('lightbox');
+        $('.lightbox').children().show();
+        //show all the rest of the figures if you click off
+        if (!$(this).parent().hasClass('lightbox')) {
+            $('figure').show();
+            $('figcaption').hide();
+        }
+       
+    });
+    $('.close').on('click', function () {
+        $(this).parent().removeClass('lightbox');
+        $('figure').show();
+        $('figcaption').hide();
+        $('.close').hide();
         var inputText = $('input').val();
         searchString(inputText);
     });
