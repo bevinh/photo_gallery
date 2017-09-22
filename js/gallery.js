1c088d13d2b63c59b4512f3972948c108595ac9e
Bevin Hernandez <bevin.hernandez@gmail.com>
2017-09-22 11:42:09 -0400

    Refactoring to use JSON

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,92 @@
 (function () {
     'use strict';
-    //get all the currently visible figures & figcaptions
-    var visiblefigures = $('figure:visible'),
-    figcaptions = $('figcaption');
+    
+    var images = [
+        {
+            "url": "/images/01.jpg", 
+            "alttext": "Hay Bales",
+            "figcaptiontext": "I love hay bales. Took this snap on a drive through the countryside past some straw fields"
+        },
+        {
+            "url": "/images/02.jpg",
+            "alttext": "Lake",
+            "figcaptiontext": "The lake was so calm today. We had a great view of the snow on the mountains from here."
+        },
+        {
+            "url": "/images/03.jpg",
+            "alttext": "Canyon",
+            "figcaptiontext": "I hided to the top of the mountain and got this picture of the canyon and trees below."
+        },
+        {
+            "url": "/images/04.jpg",
+            "alttext": "Iceberg",
+            "figcaptiontext": "It was amazing to see an iceberg up close, it was so cold but didn't snow today."
+        },
+        {
+            "url": "/images/05.jpg",
+            "alttext": "Desert",
+            "figcaptiontext": "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
+        },
+        {
+            "url": "/images/06.jpg",
+            "alttext": "Fall",
+            "figcaptiontext": "Fall is coming, I love when the leaves on the trees start to change color."
+        },
+        {
+            "url": "/images/07.jpg",
+            "alttext": "Plantation",
+            "figcaptiontext": "I drove past this plantation yesterday, everything is so green!"
+        },
+        {
+            "url": "/images/08.jpg",
+            "alttext": "Dunes",
+            "figcaptiontext": "My summer vacation to the Oregon Coast. I love the sandy dunes!"
+        },
+        {
+            "url": "/images/09.jpg",
+            "alttext": "Countryside Lane",
+            "figcaptiontext": "We enjoyed a quiet stroll down this countryside lane."
+        },
+        {
+            "url" : "/images/10.jpg",
+            "alttext": "Sunset",
+            "figcaptiontext": "Sunset at the coast! The sky turned a lovely shade of orange."
+        },
+        {
+            "url" : "/images/11.jpg",
+            "alttext": "Cave",
+            "figcaptiontext": "I did a tour of a cave today and the view of hte landscape below was breathtaking."
+        },
+        {
+            "url": "/images/12.jpg",
+            "alttext": "Bluebells",
+            "figcaptiontext": "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
+        }
+        
+    ];
+        
    
-    //hide all the figcaptions when the page loads
-    $.each(figcaptions, function () {
-        $(this).hide();
-    });
-    //hide all the close/previous/next buttons
-    $('.close').hide();
-    $('.prev').hide();
-    $('.next').hide();
+
+    function populatePage() {
+        //iterate over the json object
+        for (var key in images) {
+            //write the html
+            $('#photos').append(
+                "<figure id='" +
+                key +
+                "'><img src='" + 
+               images[key].url + 
+                "' alt='" + 
+               images[key].alttext + 
+                "'/><figcaption>" +
+                images[key].figcaptiontext + 
+                "</figcaption></figure>")
+        };
+    }
+    populatePage();
+    //keep the figcaptions but hide them so that they don't add extra space in the display
+    $('figcaption').hide();
+    
     //function for finding the text in the figures
     function searchString(cap) {
         //Hide all the figures
@@ -37,57 +112,41 @@
             }
         });
     }
-    function showLightBox(index) {
-         //hide all the figures
-        $('figure').hide();
-        //show just the one you clicked
-        console.log(index);
-        $(index).show();
-        $(index).children().show();
-        //toggle the class on or off
-        $(index).parent().toggleClass('lightbox');
-        $('.lightbox').children().show();
-        //show all the rest of the figures if you click off
-        if (!$(index).parent().hasClass('lightbox')) {
-            $('figure').show();
-            $('figcaption').hide();
-        }
-        //hide the next and previous arrows if you only have one image filtered
-       if (visiblefigures.length === 1) {
-            $('.prev').hide();
-            $('.next').hide();
-       }
-    }
-    //check the input field for input, and search the alt and images to see if it's there
+     //check the input field for input, and search the alt and images to see if it's there
     $("input").keyup(function () {
         var inputText = $('input').val();
         searchString(inputText);
-        //change the array of visible figures to return only the filtered results
-        visiblefigures = $('figure:visible');
+    });
+    
+     //open the lightbox       
+    function openLightbox(index){
+        //append the lightbox, with the appropriate images
+         $('#photos').append(
+                    '<div id="lightbox" class="lightbox">' +
+                    '<span class="close cursor">&times;</span>' +
+                    '<div class="lightbox-content">' +
+                    '<div class="slides">' + 
+                    '<img src="' +
+                    images[index].url +
+                    '" alt="' +
+                    images[index].alttext +
+                    '"style="width:100%">' +
+                    '<a class="prev" onclick="plusSlides(-1)">&#10094;</a>' +
+                    '<a class="next" onclick="plusSlides(1)">&#10095;</a>' +
+                    '<div class="caption-container"><p id="caption">' +
+                    images[index].figcaptiontext +
+                    '</p></div></div>'
+            );
         
-        visiblefigures.each(function(index) {
-            $(this).on('click', function() {
-                showLightBox(index)
-            })
-        })     
-    });
+        $('.close').on("click", function(){
+         $("#lightbox").remove();
+        });
+    }
     
-    
-    
-    //lightbox show/hide
-    $('figure').on('click', function () {
-        var index = $(this);
-        showLightBox(index); 
-    });
-    
-    //Close function goes on div to remove lightbox if you click anywhere in it
-    $('.close').on('click', function () {
-        $(this).parent().removeClass('lightbox');
-        $('figure').show();
-        $('figcaption').hide();
-        $('.close').hide();
-        var inputText = $('input').val();
-        searchString(inputText);
-    });
+    //click action to open the lightbox
+    $('figure').on("click", function(){
+        var index = $(this).attr('id')
+        openLightbox(index);
+    })
     
 }());
