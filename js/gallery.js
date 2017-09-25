5ee514ab286302bb9b1471ead82caa3be9754aea
Bevin Hernandez <bevin.hernandez@gmail.com>
2017-09-25 09:27:53 -0400

    tidied css & js, added 1024px > styles

--- js/gallery.js
+++ js/gallery.js
@@ -4,80 +4,80 @@
         newImages = [],
         unique = [],
         images = [
-        {
-            "id" : "01",
-            "url": "/images/01.jpg", 
-            "alttext": "Hay Bales",
-            "figcaptiontext": "I love hay bales. Took this snap on a drive through the countryside past some straw fields"
-        },
-        {
-            "id" : "02",
-            "url": "/images/02.jpg",
-            "alttext": "Lake",
-            "figcaptiontext": "The lake was so calm today. We had a great view of the snow on the mountains from here."
-        },
-        {
-            "id" : "03",
-            "url": "/images/03.jpg",
-            "alttext": "Canyon",
-            "figcaptiontext": "I hided to the top of the mountain and got this picture of the canyon and trees below."
-        },
-        {
-            "id" : "04",
-            "url": "/images/04.jpg",
-            "alttext": "Iceberg",
-            "figcaptiontext": "It was amazing to see an iceberg up close, it was so cold but didn't snow today."
-        },
-        {
-            "id" : "05",
-            "url": "/images/05.jpg",
-            "alttext": "Desert",
-            "figcaptiontext": "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
-        },
-        {
-            "id" : "06",
-            "url": "/images/06.jpg",
-            "alttext": "Fall",
-            "figcaptiontext": "Fall is coming, I love when the leaves on the trees start to change color."
-        },
-        {
-            "id" : "07",
-            "url": "/images/07.jpg",
-            "alttext": "Plantation",
-            "figcaptiontext": "I drove past this plantation yesterday, everything is so green!"
-        },
-        {
-            "id" : "08",
-            "url": "/images/08.jpg",
-            "alttext": "Dunes",
-            "figcaptiontext": "My summer vacation to the Oregon Coast. I love the sandy dunes!"
-        },
-        {
-            "id" : "09",
-            "url": "/images/09.jpg",
-            "alttext": "Countryside Lane",
-            "figcaptiontext": "We enjoyed a quiet stroll down this countryside lane."
-        },
-        {
-            "id" : "10",
-            "url" : "/images/10.jpg",
-            "alttext": "Sunset",
-            "figcaptiontext": "Sunset at the coast! The sky turned a lovely shade of orange."
-        },
-        {
-            "id" : "11",
-            "url" : "/images/11.jpg",
-            "alttext": "Cave",
-            "figcaptiontext": "I did a tour of a cave today and the view of hte landscape below was breathtaking."
-        },
-        {
-            "id" : "12",
-            "url": "/images/12.jpg",
-            "alttext": "Bluebells",
-            "figcaptiontext": "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
-        }
+            {
+                "id" : "01",
+                "url" : "/images/01.jpg",
+                "alttext": "Hay Bales",
+                "figcaptiontext": "I love hay bales. Took this snap on a drive through the countryside past some straw fields"
+            },
+            {
+                "id" : "02",
+                "url": "/images/02.jpg",
+                "alttext": "Lake",
+                "figcaptiontext": "The lake was so calm today. We had a great view of the snow on the mountains from here."
+            },
+            {
+                "id" : "03",
+                "url": "/images/03.jpg",
+                "alttext": "Canyon",
+                "figcaptiontext": "I hided to the top of the mountain and got this picture of the canyon and trees below."
+            },
+            {
+                "id" : "04",
+                "url": "/images/04.jpg",
+                "alttext": "Iceberg",
+                "figcaptiontext": "It was amazing to see an iceberg up close, it was so cold but didn't snow today."
+            },
+            {
+                "id" : "05",
+                "url": "/images/05.jpg",
+                "alttext": "Desert",
+                "figcaptiontext": "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
+            },
+            {
+                "id" : "06",
+                "url": "/images/06.jpg",
+                "alttext": "Fall",
+                "figcaptiontext": "Fall is coming, I love when the leaves on the trees start to change color."
+            },
+            {
+                "id" : "07",
+                "url": "/images/07.jpg",
+                "alttext": "Plantation",
+                "figcaptiontext": "I drove past this plantation yesterday, everything is so green!"
+            },
+            {
+                "id" : "08",
+                "url": "/images/08.jpg",
+                "alttext": "Dunes",
+                "figcaptiontext": "My summer vacation to the Oregon Coast. I love the sandy dunes!"
+            },
+            {
+                "id" : "09",
+                "url": "/images/09.jpg",
+                "alttext": "Countryside Lane",
+                "figcaptiontext": "We enjoyed a quiet stroll down this countryside lane."
+            },
+            {
+                "id" : "10",
+                "url" : "/images/10.jpg",
+                "alttext": "Sunset",
+                "figcaptiontext": "Sunset at the coast! The sky turned a lovely shade of orange."
+            },
+            {
+                "id" : "11",
+                "url" : "/images/11.jpg",
+                "alttext": "Cave",
+                "figcaptiontext": "I did a tour of a cave today and the view of hte landscape below was breathtaking."
+            },
+            {
+                "id" : "12",
+                "url": "/images/12.jpg",
+                "alttext": "Bluebells",
+                "figcaptiontext": "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
+            }
         
-    ];
+        ];
         
    
 
@@ -102,6 +102,7 @@
     $('figcaption').hide();
      
     function filterImages(){
+        //make sure you have unqiue images
         slides = $.unique(slides);
         
         function findImages(item, index){
@@ -177,18 +178,19 @@
     
      //open the lightbox       
     function openLightbox(index){
+        //make sure the array isn't full of duplicates
         filterImages()
         var lightboxImages = [];
+        
+        //make sure lightboxImages has the correct image array according to what is displayed on the screen
         if ($('input').val().length == 0) {
            lightboxImages = images;
-            console.log("I have lightboxImages that are full of images")
         } 
-        
+        //make sure it's set to the newImages array if the search box is empty
         if(newImages.length > 0 && $('input').val().length > 0){
             lightboxImages = newImages;
-            console.log("i have lightboxImages that are full of newImages")
         }
-         console.log(lightboxImages)
+        
          //find the image that matches the index
        var image = lightboxImages.find(image => image.id === index);
         //append the lightbox, with the appropriate images
@@ -208,6 +210,7 @@
                     image.figcaptiontext +
                     '</p></div></div>'
             );
+        //hide or show the previous/next buttons when the image is initially shown.
         if (lightboxImages.indexOf(image) === 0) {
             $('.prev').hide();
         }
@@ -217,28 +220,37 @@
         
         var slideno = lightboxImages.indexOf(image);
         
+        //next button for the slider
         $('.next').on("click", function(){
             slideno = slideno + 1;
+            //set the new image's source, alttext, and caption
             $('#lightboximage').attr("src", lightboxImages[slideno].url)
             $('#lightboximage').attr("alt", lightboxImages[slideno].alttext)
             $('#caption').text(lightboxImages[slideno].figcaptiontext)
+            
+            //hide or show the appropriate lightbox buttons
             if (lightboxImages.indexOf(lightboxImages[slideno]) > 0) {
-            $('.prev').show();
+                $('.prev').show();
             }
             if (lightboxImages.indexOf(lightboxImages[slideno]) === 0) {
                  $('.prev').hide();
             }
              if (lightboxImages.indexOf(lightboxImages[slideno]) === lightboxImages.length - 1) {
-            $('.next').hide();
+                $('.next').hide();
             }
         })
+        
+        //previous button for the slider
         $('.prev').on("click", function(){
             slideno = slideno - 1;
+            //set the new image's source, alttext, and caption
             $('#lightboximage').attr("src", lightboxImages[slideno].url)
             $('#lightboximage').attr("alt", lightboxImages[slideno].alttext)
             $('#caption').text(lightboxImages[slideno].figcaptiontext)
+            
+            //hide or show the appopriate lightbox buttons
             if (lightboxImages.indexOf(lightboxImages[slideno]) > 0) {
-            $('.prev').show();
+                $('.prev').show();
             }
             if (lightboxImages.indexOf(lightboxImages[slideno]) === 0) {
                  $('.prev').hide();
@@ -251,6 +263,7 @@
             $('.next').hide();
             }
         })
+        
         //close action to remove the lightbox once it is opened
         $('.close').on("click", function(){
          $("#lightbox").remove();
